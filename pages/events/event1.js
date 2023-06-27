e9c43a9cd7134474392ff37f699e29521942932c
jennyyang <yzhenjuan@gmail>
2023-06-27 10:37:56 +1000

    Basic build

---
+++ pages/events/event1.js
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1>Event Page</h1>;
+};
+
+export default Page;
---
+++ pages/events/index.js
@@ -0,0 +1,23 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Events Page</h1>
+      <div>
+        <a href=''>
+          <img />
+          <h2> Events in London</h2>
+        </a>
+        <a href=''>
+          <img />
+          <h2> Events in San Francisco</h2>
+        </a>
+        <a href=''>
+          <img />
+          <h2> Events in Barcelona</h2>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
